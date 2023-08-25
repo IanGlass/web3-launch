6958ceb15724c92170894442d9133ac72283d672
Ian Glass <igla626@aucklanduni.ac.nz>
2023-08-25 16:10:39 +1200

    Add a header and wrap with layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import factory from '../ethereum/factory';
+import { Card, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import Layout from '../components/Layout';
 
 export async function getServerSideProps() {
   return {
     props: {
-      campaigns: await factory.methods.getDeployedCampaigns().call()
-    }
-  }
+      campaigns: await factory.methods.getDeployedCampaigns().call(),
+    },
+  };
 }
 
 export default function Home({ campaigns }) {
-  console.log(campaigns);
-  return <main>
-    <div>hello</div>
-  </main>;
+  const renderCampaigns = () =>
+    campaigns.map((address) => ({
+      header: address,
+      description: <a>View Campaign</a>,
+      meta: '',
+      fluid: true,
+    }));
+
+  return (
+    <Layout>
+      <main>
+        <div>
+          <h3>Open Campaigns</h3>
+          <Card.Group items={renderCampaigns()} />
+          <Button content='Create Campaign' icon='add circle' primary />
+        </div>
+      </main>
+    </Layout>
+  );
 }
